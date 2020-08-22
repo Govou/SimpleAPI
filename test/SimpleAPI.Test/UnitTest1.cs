c01576205beabeedebad2cb8b6001f6bf52c7654
Govou <31394194+Govou@users.noreply.github.com>
2020-08-22 12:02:16 +0100

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using System.Linq;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void GetReturnsValues()
+        {
+            var returnValue = controller.Get().ToList();
+            Assert.NotNull(returnValue);
+           // Assert.DoesNotContain("Freezing", returnValue[0].Summary);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
